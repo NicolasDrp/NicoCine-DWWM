38eb37a8e1a6c2f288f3f3bc1e29a05bb7ccb315
NicolasDrp <drapier.nicolas07@gmail.com>
2023-04-14 16:23:58 +0200

    contact fonctionnel mais moche

---
+++ contact.js
@@ -0,0 +1,7 @@
+btncontact.addEventListener("click", function() {
+    contact.style.display = "block";
+  });
+
+  croixContact.addEventListener("click",function(){
+      contact.style.display = "none";
+  })
