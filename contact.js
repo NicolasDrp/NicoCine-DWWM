748dcfba649c76a5450a033351c8eb94071d14a4
NicolasDrp <drapier.nicolas07@gmail.com>
2023-04-19 16:04:07 +0200

    responsive terminer

--- contact.js
+++ contact.js
@@ -1,7 +1,23 @@
-btncontact.addEventListener("click", function() {
+// formulaire de contact
+btncontact.addEventListener("click", function () {
     contact.style.display = "block";
-  });
+});
 
-  croixContact.addEventListener("click",function(){
-      contact.style.display = "none";
-  })
+croixContact.addEventListener("click", function () {
+    contact.style.display = "none";
+})
+
+
+// menu burger
+menuBurger.addEventListener("click", function () {
+    header.style.display = "initial";
+    menuBurger.style.display = "none";
+    croixHeader.style.display = "initial";
+});
+
+croixHeader.addEventListener("click", function () {
+    header.style.display = "none";
+    croixHeader.style.display = "none";
+    menuBurger.style.display = "initial";
+})
+
